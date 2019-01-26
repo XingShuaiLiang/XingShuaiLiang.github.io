83533fdcaf052d86051c4a456611b6bca905da63
XingShuaiLiang <1004968367@qq.com>
2019-01-26 11:19:31 +0800

    修改标题栏是否显示或透明的js代码到main.js里面，经过测试可满足当前需求，以后可加入标题栏延迟渐变功能

--- js/main.js
+++ js/main.js
@@ -30,25 +30,43 @@
 
 //////////////////////////back to top////////////////////////////
 (function() {
-  var backToTop = document.querySelector('.back-to-top')
-  var backToTopA = document.querySelector('.back-to-top a')
-  // console.log(backToTop);
-  window.addEventListener('scroll', function() {
+	var backToTop = document.querySelector('.back-to-top')
+	var backToTopA = document.querySelector('.back-to-top a')
+	var t = 0;//判断上滚动
+	var d = 0;//判断下滚动
+	// console.log(backToTop);
+	window.addEventListener('scroll', function() {
+		//判断浏览器页面上下滚动来确定导航栏是否显示
+		d = document.documentElement.scrollTop;
+		if (t > d)
+		{
+			//如果t大于p就是往上滚动，反之
+			document.getElementById("top").style.visibility = "visible";
+			document.getElementById("top").style.background = "rgba(255, 255, 255, 0.9)";
+		}
+		else
+		{
+			document.getElementById("top").style.visibility = "hidden";
+		}
+		t = d;
+		// 页面顶部滚进去的距离来确定是否显示回顶部按钮和导航栏透明
+		var scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop)
 
-    // 页面顶部滚进去的距离
-    var scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop)
+		if (scrollTop > 200) 
+		{
+			backToTop.classList.add('back-to-top-show')
+		} 
+		else 
+		{
+			backToTop.classList.remove('back-to-top-show')
+			document.getElementById("top").style.background = "rgba(255, 255, 255, 0)";
+		}
+	})
 
-    if (scrollTop > 200) {
-      backToTop.classList.add('back-to-top-show')
-    } else {
-      backToTop.classList.remove('back-to-top-show')
-    }
-  })
-
-  // backToTopA.addEventListener('click',function (e) {
-  //     e.preventDefault()
-  //     window.scrollTo(0,0)
-  // })
+	// backToTopA.addEventListener('click',function (e) {
+	//     e.preventDefault()
+	//     window.scrollTo(0,0)
+	// })
 }());
 
 //////////////////////////hover on demo//////////////////////////////
